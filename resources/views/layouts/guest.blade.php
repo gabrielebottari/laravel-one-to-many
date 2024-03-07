defb00a15b1be7715c48f662b9069c3b1525639b
gabrielebottari <gabriele.bottari1@gmail.com>
2024-03-07 17:46:51 +0100

    END exercise W/ BONUS

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('page-title') | {{ config('guest.name', 'Boolfolio') }}</title>
 
         <!-- Scripts -->
         @vite('resources/js/app.js')
